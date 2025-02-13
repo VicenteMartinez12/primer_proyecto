41ca4355931dbedcfe2f8060ebff4cc6f25fb63b
Vicente Lara Martinez <76573231+VicenteMartinez12@users.noreply.github.com>
2025-02-13 16:44:10 -0600

    3

---
+++ mi_app/mi_app/__init__.py
@@ -0,0 +1,7 @@
+# mi_app/__init__.py
+
+# Importar el modelo Producto si deseas que sea accesible desde `mi_app`
+
+
+# Asegurar que Django reconozca correctamente la aplicación
+default_app_config = "mi_app.apps.MiAppConfig"
